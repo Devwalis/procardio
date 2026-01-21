c9fac1651776267fcea8f67e1e0d7c049b683dab
Walisson <Walissonoliveira1@outlook.com>
2026-01-21 17:43:43 -0300

    Metodo deletar consulta por id

--- src/main/java/br/com/procardio/api/controller/ConsultaController.java
+++ src/main/java/br/com/procardio/api/controller/ConsultaController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -63,6 +65,21 @@ public class ConsultaController {
 
 
 }
+
+
+@DeleteMapping("/{id}")
+@PreAuthorize("hasRole('ADMIN')")
+public ResponseEntity<Void> deletarConsulta(@PathVariable Long id){
+    Consulta consulta = consultaService.buscarConsultaPorId(id);
+    if(consulta == null){
+        return ResponseEntity.notFound().build();
+    }
+    consultaService.deletarConsulta(id);
+    return ResponseEntity.noContent().build();
+}
+
+
+
 
 
      
