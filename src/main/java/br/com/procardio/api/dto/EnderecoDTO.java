319732de0a0f328ee334390a4f4c92c5680a27f3
Walisson <Walissonoliveira1@outlook.com>
2026-02-02 13:51:22 -0300

    EnderecoDTO

--- src/main/java/br/com/procardio/api/dto/EnderecoDTO.java
+++ src/main/java/br/com/procardio/api/dto/EnderecoDTO.java
@@ -1,7 +1,15 @@
 package br.com.procardio.api.dto;
 
-public class EnderecoDTO {
+public record EnderecoDTO(
+
+    String cep,
+    String logradouro,
+    String bairro,
+    String localidade,
+    String uf
+) {
+}
+
 
     
-    
-}
+
